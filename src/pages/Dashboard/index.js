fbba8f9cb6ec677b422c27dffee0d20b55ed3eb9
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-10-13 17:18:56 -0300

    Appointment list - Stylizing

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import {Text} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {Container, Title, List} from './styles';
+import Appointment from '~/components/Appointments';
+
+const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
-  return <Background />;
+  return (
+    <Background>
+      <Container>
+        <Title>Appointments</Title>
+
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({item}) => <Appointment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 // Customizing TabBarNavigation for this route

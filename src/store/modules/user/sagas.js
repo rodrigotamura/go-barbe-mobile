9e5b1006f8b08cd98cd221e22a94190c453c7ee6
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-10-13 18:55:23 -0300

    Updating profile.

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -6,13 +6,12 @@ import {updateProfileSuccess, updateProfileFailure} from './actions';
 
 export function* updateProfile({payload}) {
   try {
-    const {name, email, avatar_id, ...rest} = payload.data;
+    const {name, email, ...rest} = payload.data;
 
     // It will join two objects
     const profile = {
       name,
       email,
-      avatar_id,
       ...(rest.oldPassword ? rest : {}),
     };
 

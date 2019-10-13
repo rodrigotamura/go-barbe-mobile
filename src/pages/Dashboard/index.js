4dcba1dd391fdd3b7841e53f7b48c9bb5872e989
Rodrigo Tamura <rodrigotamura@hotmail.com>
2019-10-13 16:46:38 -0300

    Bottom tab navigation stylization

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  return <Text>Dashboard</Text>;
+  return <Background />;
 }
+
+// Customizing TabBarNavigation for this route
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Appointments',
+  tabBarIcon: ({tintColor}) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+  /**
+   * {tintColor} comes from routes.js
+   */
+};
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 export default (isSigned = false) =>
   createAppContainer(
@@ -15,9 +16,23 @@ export default (isSigned = false) =>
           SignUp,
         }),
 
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true, // keyboard overlay tabs when opened
+              activeTintColor: '#FFF', // highlight selected route
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#402845',
+                borderTopColor: 'transparent',
+              },
+            },
+          },
+        ),
       },
       {
         initialRouteName: isSigned ? 'App' : 'Sign',
